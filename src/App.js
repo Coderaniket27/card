373e0a8e6ff14fae0b44c69ad408f580695f5ba8
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2022-12-11 14:00:37 +0530

    no

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
+
 
 function App() {
+  const[Name,SetName]=useState("")
+  const [count, setCount] = useState(" ");
+const[id,setid]=useState(" ")
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Card">
+        <div className="Card-inner">
+
+<div  style={{textAlign:"center", marginTop:"20px"}}className='name'>
+  {Name}
+
+</div>
+<div style={{textAlign:"center", marginTop:"20px"}}  className='name'>
+
+  {count}
+
+</div>
+<div style={{textAlign:"center", marginTop:"20px"}}  className='names'>
+
+  {id}
+
+</div>
+        </div>
+      </div>
+      <label>
+    Name:
+      <input type="text" placeholder="Firstname" value={Name} onChange={(e)=>{
+        SetName(e.target.value)
+      }} />
+       </label>
+      <br/>
+      <br/>
+      <label>
+        Phone:
+      <input type="text" placeholder="no" value={count} onChange={(e)=>{
+        setCount(e.target.value)
+      }}  />
+ </label>
+ <button onClick={(e)=>{setid(Math.floor(Math.random() * 10000000))}}>Submit</button>
     </div>
   );
 }
