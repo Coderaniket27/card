8fad1d39b895ca897301764e0079e58131f4647d
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2022-12-21 20:58:52 +0530

    jo

--- src/App.js
+++ src/App.js
@@ -9,40 +9,13 @@ const[id,setid]=useState(" ")
 
   return (
     <div className="App">
-      <div className="Card">
-        <div className="Card-inner">
+    <div className='Card'>
+      <a href='https://www.mediassist.in/'>doctor click</a>
+    </div>
+        
 
-<div  style={{textAlign:"center", marginTop:"20px"}}className='name'>
-  {Name}
 
-</div>
-<div style={{textAlign:"center", marginTop:"20px"}}  className='name'>
 
-  {count}
-
-</div>
-<div style={{textAlign:"center", marginTop:"20px"}}  className='names'>
-
-  {id}
-
-</div>
-        </div>
-      </div>
-      <label>
-    Name:
-      <input type="text" placeholder="Firstname" value={Name} onChange={(e)=>{
-        SetName(e.target.value)
-      }} />
-       </label>
-      <br/>
-      <br/>
-      <label>
-        Phone:
-      <input type="text" placeholder="no" value={count} onChange={(e)=>{
-        setCount(e.target.value)
-      }}  />
- </label>
- <button onClick={(e)=>{setid(Math.floor(Math.random() * 10000000))}}>Submit</button>
     </div>
   );
 }
