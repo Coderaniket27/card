609a22a4ec7e50c85f5dad2615c1a1299203b55b
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2022-12-28 13:24:54 +0530

    full

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import './App.css';
 import Nav from './Component/Nav';
 import Banner from './Component/Banner';
+import Aboutus from './Component/Aboutus';
 function App() {
   return (
     <div >
         <Nav/>
 
 <Banner/>
+<Aboutus/>
+
+
+
 
     </div>
   );
--- src/Component/Nav.js
+++ src/Component/Nav.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Hamburger from 'hamburger-react'
 import { useState } from 'react'
+import {Link} from 'react-scroll'
+
 
 const Nav = () => {
   const[list,showlist]=useState(false)
-  const navlists=["Home","About Us","Products","Benefits","Offers","Lab Test","Hospital","Ambulance","Doctor Appointment","Home Care","Health Checkup","Mental Health & Fitness"]
+  const[list1,setlist1]=useState(false)
+  const navlists=["Home","About Us","Benefits","Offers","Lab Test","Hospital","Ambulance","Doctor Appointment","Home Care","Health Checkup","Mental Health & Fitness"]
 
-  const navlist=["Home","About Us","Products","Benefits","Offers"]
+  // const navlist=["Home","About Us","Products","Benefits","Offers"]
   return (
     <div className='NavBar' style={{"width":"100%"}}>
         <div className='NavFull'>
@@ -24,20 +27,69 @@ showlist(false)  }
 
           </div>
 <ul className='Navitems'>
- {navlist.map((e,i) =>{
-  return(<li className="hovers"style={{paddingRight:"50px"}}key={i}>{e}</li>)
- })}
+  <li className="hovers"style={{paddingRight:"50px"}}>Home</li>
+<li className="hovers"style={{paddingRight:"50px"}}>  <Link activeClass="active" to="about" spy={true} smooth={true}>About Us</Link></li>
+         
+<li onClick={(e) =>{
+  setlist1(!list1)
+}} className="hovers"style={{paddingRight:"50px"}}>Products
+
+ </li>
+
+  <li className="hovers"style={{paddingRight:"50px"}}>Benefits</li>
+  <li className="hovers"style={{paddingRight:"50px"}}>Offers</li>
+
 </ul>
 </div> 
+{list1?<div className='list-item1'>
+<ul style={{  listStyle: "none"}}>
+  <li onClick={(e) =>{
+  setlist1(!list1)
+}} className='hamburger-list'>
+   Lab Test
+  </li>
+  <li onClick={(e) =>{
+  setlist1(!list1)
+}} className='hamburger-list'>
+   Hospital
+  </li>
+  <li onClick={(e) =>{
+  setlist1(!list1)
+}}className='hamburger-list'>
+   Ambulance
+  </li>
+  <li onClick={(e) =>{
+  setlist1(!list1)
+}}className='hamburger-list'>
+  Doctor Appointment
+  </li>
+  <li onClick={(e) =>{
+  setlist1(!list1)
+}}  className='hamburger-list'>
+  Home Care
+  </li>
+  <li  onClick={(e) =>{
+  setlist1(!list1)
+}} className='hamburger-list'>
+  Health Checkup
+  </li>
+  <li  onClick={(e) =>{
+  setlist1(!list1)
+}} className='hamburger-list'>
+  Mental Health & Fitness  </li>
+</ul>
+</div>:""}
 {list?<div className='list-item'>
   <ul style={{listStyle:"none"}}>
+    
     {navlists.map((e,i)=>{
-      return (<li className='hamburger-list' key={i}>{e}</li>)
+      return (<li className='hamburger-list' key={i}> <Link activeClass="active" to="about" spy={true} smooth={true}>{e}</Link></li>)
     })}
 
   </ul>
 
 </div>:""}
+
           </div>
             
     </div>
