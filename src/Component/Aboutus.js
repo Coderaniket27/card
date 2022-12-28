6a5f577fb5e56ccbc81aeee5648402c51f2edb62
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2022-12-28 13:27:50 +0530

    ok

--- src/Component/Aboutus.js
+++ src/Component/Aboutus.js
@@ -6,7 +6,7 @@ const Aboutus = () => {
     <div id="about" className='About'>
         <div className='About-Brief'>
             <div className='About-head'>About Us</div>
-    We are engaged in a fervent journey to  establish ourselves as a premium-quality health care service provider at the lowest and most affordable price.
+    We are engaged in a fervent journey to  establish ourselves as a <i>premium-quality</i> health care service provider at the lowest and most affordable price.
     We believe that quality and lowest cost are  not mutually exclusive when it comes to healthcare delivery, but we are trying to make them as compatible as possible.
     We are a technologically advanced healthcare platform that offers professional diagnostic healthcare check-ups, among other services.This is something you can do from the comfort of your own home or office.
     Driven by the passion to provide care, impelled by an unwavering focus on quality, and & steered by ground-breaking artificial intelligence.
