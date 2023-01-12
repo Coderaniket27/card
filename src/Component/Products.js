505cc2e33c3912e905207f4d36585d018d094feb
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2023-01-13 00:17:50 +0530

    all

--- src/Component/Products.js
+++ src/Component/Products.js
@@ -17,6 +17,84 @@ const Products = () => {
   THE GROWING NEED FOR ONSITE CLINICS
 
   </div>
+  <div className='Product-Brief'>
+  As we watch, the switch in the working condition is being changed to reflect the current situation.We are offering healthcare solutions to corporate employees and
+  students to give them a healthy healthcare environment. Onsite clinics are a long term solution to maintain employees' and students' health, improve cost-effectiveness
+  in terms of absenteeism, and also provide a stress free work environment and timely and easy access to primary and acute care.
+  </div>
+  <div className='Pro-Package'>
+    <div className='Pro-pack1'>
+      <ul>
+        <li>
+        Qualified Nurse Deputation.
+        </li>
+        <li>
+        Online and Offline Doctor Consultation as and when required.
+        </li>
+        <li>
+        Emergency Ambulance Services
+        </li>
+        <li>
+        OTC medicines.
+        </li>
+        <li>
+        Medical furniture and Fixtures.
+        </li>
+        <li>
+        Medical Consumable and Disposable.
+        </li>
+        <li>
+        Assistance in Hospital admission and at home Lab Services.
+        </li>
+        <li>
+        Accidental Cover of Rs.100000 per person.
+        </li>
+      </ul>
+
+    </div>
+    <div className='Pro-pack2'>
+      <ul>
+        <li>
+        Student/ Employee Health Records
+        </li>
+        <li>
+        Qualified Nurse Deputation.
+        </li>
+        <li>
+        Online and Offline Doctor Consultation as and when required.
+        </li>
+        <li>
+        Emergency Ambulance Services
+        </li>
+        <li>
+        Medical furniture and Fixtures.
+        </li>
+        <li>
+        Medical Consumable and Disposable.
+        </li>
+        <li>
+        OTC medicines.
+        </li>
+        <li>
+        Assistance in Hospital admission and at home Lab Services.
+        </li>
+        <li>
+        Accidental Cover of Rs.300000 per person.
+        </li>
+        <li>
+        Annual Complete Health Checkup Camp including (Eye,ENT,Dental,General
+Medicine / Pediatrics or as per requirement).
+        </li>
+        <li>
+        Mental Wellness Sessions (Yoga,Meditation,Counselling Session)
+        </li>
+        <li>
+        Physical Wellness Session (Physiotherapy,Diet,Zumba etc).
+        </li>
+      </ul>
+
+    </div>
+  </div>
 </div>
       </div>
     </div>
