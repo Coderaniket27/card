1babbd6c68412cd3d67787c23d818598fbfa36b2
Superstar27 <91425528+Superstar27@users.noreply.github.com>
2023-09-17 23:08:19 +0530

    merntal health and 4 brief change

--- src/Component/AllBenefits.js
+++ src/Component/AllBenefits.js
@@ -514,10 +514,7 @@ const AllBenefits = () => {
             Lab Test
           </div>
           <div style={{ color: "white" }} className="Paragraphs">
-            In Bihar, you can schedule emergency hospital stays and operations
-            from anywhere. Hospitals are readily available, and{" "}
-            <b>Nirog Sansar</b> staff will assist you in booking a suitable
-            slot.  
+          Book lab tests from our network lab with free sample collection from any location and at any time .
           </div>
           <div className="points">
             <div className="points-icon">
@@ -527,9 +524,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Bihar</div>
               <div style={{color:"white"}}className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                With our service partners and home collection centres across bihar, all your employees and loved ones get a uniform experience.{" "}
               </div>
             </div>
           </div>
@@ -541,9 +536,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Certified Connection</div>
               <div style={{color:"white"}}className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                Working with only NABL and NABH-certified partners, we regularly monitor them to ensure service levels are impeccable. {" "}
               </div>
             </div>
           </div>
@@ -555,9 +548,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Discount & Cashback</div>
               <div style={{color:"white"}} className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We provide up to 100% genuine cashback and up to 50% off on lab tests, with free sample collection at your doorstep. {" "}
               </div>
             </div>
           </div>
@@ -570,10 +561,7 @@ const AllBenefits = () => {
         <div className="First-Headings">
           <div className="Headings">Radiology</div>
           <div className="Paragraphs">
-            In Bihar, you can schedule emergency hospital stays and operations
-            from anywhere. Hospitals are readily available, and{" "}
-            <b>Nirog Sansar</b> staff will assist you in booking a suitable
-            slot.  
+          You can easily schedule a nuclear screening from anywhere in Bihar.
           </div>
           <div className="points">
             <div className="points-icon">
@@ -583,9 +571,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Bihar</div>
               <div className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                For an exact assessment, we will be collaborating with the reowned  and oldest scan centres across Bihar.{" "}
               </div>
             </div>
           </div>
@@ -597,9 +583,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Certified Connection</div>
               <div className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We will be collaborating with the city's reowned scan centres to ensure the validity of the scan reports, and we will also be closely observing them to make sure their service standards are flawless.{" "}
               </div>
             </div>
           </div>
@@ -611,9 +595,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Discount & Cashback</div>
               <div className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                Along with offering discounts of up to 50% on all scans to <b>Nirog sansar</b> card holder, we'll also make sure that the scans' quality isn't compromised for the sake of the savings.{" "}
               </div>
             </div>
           </div>
@@ -628,10 +610,7 @@ const AllBenefits = () => {
             Ambulance
           </div>
           <div style={{ color: "white" }} className="Paragraphs">
-            In Bihar, you can schedule emergency hospital stays and operations
-            from anywhere. Hospitals are readily available, and{" "}
-            <b>Nirog Sansar</b> staff will assist you in booking a suitable
-            slot.  
+          Call an ambulance in Bihar from any location we will arrive in less than <i><b>20 minutes</b></i>  because every second counts.
           </div>
           <div className="points">
             <div className="points-icon">
@@ -641,9 +620,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Bihar</div>
               <div style={{color:"white"}}className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                Working with 100+ professionals and regularly checking their performance to guarantee top-notch service levels.{" "}
               </div>
             </div>
           </div>
@@ -655,9 +632,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Certified Connection</div>
               <div style={{color:"white"}}className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We provide all necessary facilities when the emergency occurs, we will provide in our certified ambulance. {" "}
               </div>
             </div>
           </div>
@@ -669,9 +644,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Discount & Cashback</div>
               <div style={{color:"white"}} className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We will offer a 30% discount and 40% cashback on our ambulance for our <b>Nirog Sansar </b>card holder. {" "}
               </div>
             </div>
           </div>
@@ -684,10 +657,7 @@ const AllBenefits = () => {
         <div className="First-Headings">
           <div className="Headings">Blood Bank</div>
           <div className="Paragraphs">
-            In Bihar, you can schedule emergency hospital stays and operations
-            from anywhere. Hospitals are readily available, and{" "}
-            <b>Nirog Sansar</b> staff will assist you in booking a suitable
-            slot.  
+          In your difficult times, we will always stand for you and organise blood from any group at any moment.  
           </div>
           <div className="points">
             <div className="points-icon">
@@ -697,9 +667,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Bihar</div>
               <div className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                With our extensive network in Bihar, we will work quickly to organise any blood group at any moment within a short period of time.{" "}
               </div>
             </div>
           </div>
@@ -711,9 +679,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Certified Connection</div>
               <div className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We guarantee that your patients will receive only pure blood because we only deal with recognised blood banks.  {" "}
               </div>
             </div>
           </div>
@@ -725,9 +691,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Discount & Cashback</div>
               <div className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We will also offer a processing charge reduction of up to 50% for blood transfusion procedures.{" "}
               </div>
             </div>
           </div>
@@ -742,10 +706,7 @@ const AllBenefits = () => {
             Doctor Consultation
           </div>
           <div style={{ color: "white" }} className="Paragraphs">
-            In Bihar, you can schedule emergency hospital stays and operations
-            from anywhere. Hospitals are readily available, and{" "}
-            <b>Nirog Sansar</b> staff will assist you in booking a suitable
-            slot.  
+          Make an appointment with your doctor anytime at your doorstep or fingertip Online and Offline 24 * 7.
           </div>
           <div className="points">
             <div className="points-icon">
@@ -755,9 +716,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Bihar</div>
               <div style={{color:"white"}}className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                Get ridd off from long quene for doctor consultation, we will book a doctor consultation offline and online just to  keep you relax, we are going to solve all your problems.{" "}
               </div>
             </div>
           </div>
@@ -769,9 +728,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Certified Connection</div>
               <div style={{color:"white"}}className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We only work with a certified, experienced and qualified doctor to make sure you are safe ans secure.{" "}
               </div>
             </div>
           </div>
@@ -783,9 +740,7 @@ const AllBenefits = () => {
               <div className="Points-Head">Discount & Cashback</div>
               <div style={{color:"white"}} className="Points-point">
                 {" "}
-                Our partnership guarantees consistent hospitalization
-                experiences for your employees and loved ones across Bihar,
-                providing accessible and reliable healthcare services.{" "}
+                We will give up to 50% discount and 50% cash back to our <strong>Nirog sansar</strong> cardholder on medical consultation. {" "}
               </div>
             </div>
           </div>
@@ -961,7 +916,7 @@ const AllBenefits = () => {
           </div>
         </div>
         <div className="First-Pics">
-          <img className="First-icons" src="home.png" alt="roof" />
+          <img className="First-icons" src="mentals.png" alt="roof" />
         </div>
       </div>
     </div>
